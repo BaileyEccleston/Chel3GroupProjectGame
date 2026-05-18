46ee3b6135decdc4241ffdbcd22ea6a69b086dbd
BOLTON.AC.UK\be1crt <baileyeccleston0@gmail.com>
2026-05-18 10:33:33 +0100

    ready for build

--- Assets/Scripts/PlayerLives.cs
+++ Assets/Scripts/PlayerLives.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLives : MonoBehaviour
 {
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+
+
+
 
     public int startingLives = 3;
     int currentLives;
@@ -21,6 +29,39 @@ public class PlayerLives : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+
+    
+    }
+
+
+
+    public void UpdateHearts()
+    {
+        if (currentLives == 3)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
+        }
+        else if (currentLives == 2)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(false);
+        }
+        else if (currentLives == 1)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+        }
+        else if (currentLives <= 0)
+        {
+            heart1.SetActive(false);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+        }
+
     }
 
     public void RestartLevel()
@@ -31,16 +72,19 @@ public class PlayerLives : MonoBehaviour
     public void IncreaseLives()
     {
         currentLives++;
+        UpdateHearts();
     }
 
     public void DecreaseLives()
     {
         currentLives--;
+        UpdateHearts();
     }
 
     public void RefillLives()
     {
         currentLives = startingLives;
+        UpdateHearts();
     }
 
 }
--- Assets/Scripts/StarManager.cs
+++ Assets/Scripts/StarManager.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class StarManager : MonoBehaviour
 {
 
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
     public int starsEarned = 0;
 
     [Header("Collectables")]
@@ -23,12 +27,42 @@ public class StarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateHearts();
         if (collectablesCollected >= collectablesToCollect && !collectedAllCollectables)
         {
             starsEarned++;
             collectedAllCollectables = true;
         }
        // Debug.Log("Stars Earned" + starsEarned);
+    }
+
+    public void UpdateHearts()
+    {
+        if (starsEarned == 3)
+        {
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(true);
+        }
+        else if (starsEarned == 2)
+        {
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(false);
+        }
+        else if (starsEarned == 1)
+        {
+            star1.SetActive(true);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        }
+        else if (starsEarned <= 0)
+        {
+            star1.SetActive(false);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        }
+
     }
 
 
